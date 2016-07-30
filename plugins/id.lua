7ea009cdd843eb9a46e7f80135019ba41789e417
DavidPro80 <davidali1045@gmail.com>
2016-07-31 00:15:32 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,6 +1,6 @@
 do
 function run(msg, matches)
-  return "Group Name: "..msg.from.id.."\nGrop ID: "..msg.to.title.."\n\nFull Name: "..(msg.from.first_name or '').."\nFirst Name: "..(msg.from.first_name or '').."\nLast Name: "..(msg.from.last_name or '').."\n\nID: "..msg.from.id.."\nUsename: @"..(msg.from.username or '').."\nPhone Number: +"..(msg.from.phone or '-----')
+  return "Group Name: "..msg.to.title.."\nGrop ID: "..msg.from.id.."\n\nFull Name: "..(msg.from.first_name or '').."\nFirst Name: "..(msg.from.first_name or '').."\nLast Name: "..(msg.from.last_name or '').."\n\nID: "..msg.from.id.."\nUsename: Telegram.me/"..(msg.from.username or '').."\nPhone Number: +"..(msg.from.phone or '-----')
 end
 return {
   description = "", 
