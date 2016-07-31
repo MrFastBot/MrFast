8dda681afe42c87937db89082ee02e7acc149035
DavidPro80 <davidali1045@gmail.com>
2016-07-31 16:29:13 +0430

    Create nerkh.lua

---
+++ plugins/nerkh.lua
@@ -0,0 +1,19 @@
+ do
+
+function run(msg, matches)
+  return "♻️Group rates with the [Sweether] Bot♻️\n➖➖➖➖➖➖➖➖➖➖\n💸SuperGroup Rates💸\nOne Month: 3,000 Toman\nTwo Month: 5,000 Toman\nThree Month: 7,000 Toman\nContinual: 14,000 Toman\n➖➖➖➖➖➖➖➖➖➖\n📡Without Online📡\n💰Pay With💰\n🔸Charge🔸\n🔹Bank Card🔹\nTo Purchase, Visit The ID⬇️\n@SweetherBotAdmin\n👑Thanks, David👑\n➖➖➖➖➖➖➖➖➖➖\n🔧Support Group Link🔧:\n⚠️ https://telegram.me/joinchat/DkgdJD-GdgpDdy_9_2ilKQ ⚠️\n➖➖➖➖➖➖➖➖➖➖\n🌐Our Channel🌐: \n🔥 https://telegram.me/SweetherNews 🔥"
+  end
+return {
+  description = " nerkh ",
+  usage = "nerkh",
+  patterns = {
+    "^Nerkh$",
+    "^nerkh$",
+    "^[#$%&/!](nerkh)$",
+    "^[#$%&/!]([Nn]erkh)$"
+  },
+  run = run
+}
+end 
+
+
