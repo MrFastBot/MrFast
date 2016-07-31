355b8fb507841d9b30b1cf817bd337763ace7876
DavidPro80 <davidali1045@gmail.com>
2016-07-31 15:27:58 +0430

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
     if matches[1] == "support" then
  local user = "user#id"..msg.from.id
- local chat = "chat#id"..
+ local chat = "chat#id"..1065776650
 
  chat_add_user(chat, user, ok_cb, false)
     end
