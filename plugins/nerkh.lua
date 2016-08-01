79f6534e8d1cd7c58062fb5f26215ea05af23c9a
DavidPro80 <davidali1045@gmail.com>
2016-08-01 15:45:43 +0430

    Update nerkh.lua

--- plugins/nerkh.lua
+++ plugins/nerkh.lua
@@ -1,7 +1,7 @@
  do
 
 function run(msg, matches)
-  return "🔃️Group rates with the [Sweether] Bot🔃\n➖➖➖➖➖➖➖➖➖➖\n⚖SuperGroup Rates⚖\nOne Month: 3,000💶\nTwo Month: 5,000💶\nThree Month: 7,000💶\nContinual: 14,000💶\n➖➖➖➖➖➖➖➖➖➖\n💢Without Online💢\n💎Pay With💎\n🔴Charge🔴\n💳Bank Card💳\nTo Purchase, Visit The ID⬇️\n@SweetherBotAdmin\n☠Thanks, David☠\n➖➖➖➖➖➖➖➖➖➖\n⚙Support Group Link⚙\n https://telegram.me/joinchat/DkgdJD-GdgpDdy_9_2ilKQ ⚠️\n➖➖➖➖➖➖➖➖➖➖\n🛡Our Channel🛡\nhttps://telegram.me/SweetherNews 🔥"                             
+  return "🔃️Group rates with the [Sweether] Bot🔃\n➖➖➖➖➖➖➖➖➖➖\n⚖SuperGroup Rates⚖\nOne Month: 3,000💶\nTwo Month: 5,000💶\nThree Month: 7,000💶\nContinual: 14,000💶\n➖➖➖➖➖➖➖➖➖➖\n💢Without Online💢\n💎Pay With💎\n🔴Charge🔴\n💳Bank Card💳\nTo Purchase, Visit The ID⬇️\n@SweetherBotAdmin\n☠Thanks, David☠\n➖➖➖➖➖➖➖➖➖➖\nSupport Group Link\nhttps://telegram.me/joinchat/DkgdJD-GdgpDdy_9_2ilKQ \n➖➖➖➖➖➖➖➖➖➖\nOur Channel\nhttps://telegram.me/SweetherNews"                             
   end
 return {
   description = " nerkh ",
