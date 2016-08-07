25c0b8889c9c107e48c72af5f5b3dacf00f2cf19
UltronTG <UltronTG@gmail.com>
2016-08-07 23:56:37 +0430

    Update charge.lua

--- plugins/charge.lua
+++ plugins/charge.lua
@@ -6,46 +6,46 @@ local function pre_process(msg)
 		timetoexpire = math.floor((tonumber(expiretime) - tonumber(now)) / 86400) + 1
 		if tonumber("0") > tonumber(timetoexpire) and not is_sudo(msg) then
 		if msg.text:match('/') then
-			return send_large_msg(get_receiver(msg), ' Group expiry date is over.\n To recharge the @SweetherBotAdmin\n Other robots in the group is active.')
+			return send_large_msg(get_receiver(msg), ' Group expiry date is over.\n To recharge the @MrDear\n Other robots in the group is active.')
 		else
 			return
 		end
 	end
 	if tonumber(timetoexpire) == 0 then
 		if redis:hget('expires0',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), ' 0 days remaining until the end of the expiry date of the group. Refer to recharge the @SweetherBotAdmin. ')
+		send_large_msg(get_receiver(msg), ' 0 days remaining until the end of the expiry date of the group. Refer to recharge the @MrDear. ')
 		redis:hset('expires0',msg.to.id,'5')
 	end
 	if tonumber(timetoexpire) == 1 then
 		if redis:hget('expires1',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '1 days remaining until the end of the expiry date of the group. Refer to recharge the @SweetherBotAdmin. ')
+		send_large_msg(get_receiver(msg), '1 days remaining until the end of the expiry date of the group. Refer to recharge the @MrDear. ')
 		redis:hset('expires1',msg.to.id,'5')
 	end
 	if tonumber(timetoexpire) == 2 then
 		if redis:hget('expires2',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '2 days remaining until the end of the expiry date of the group. Refer to recharge the @SweetherBotAdmin. ')
+		send_large_msg(get_receiver(msg), '2 days remaining until the end of the expiry date of the group. Refer to recharge the @MrDear. ')
 		redis:hset('expires2',msg.to.id,'5')
 	end
 	if tonumber(timetoexpire) == 3 then
 		if redis:hget('expires3',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '3 days remaining until the end of the expiry date of the group. Refer to recharge the @SweetherBotAdmin. ')
+		send_large_msg(get_receiver(msg), '3 days remaining until the end of the expiry date of the group. Refer to recharge the @MrDear. ')
 		redis:hset('expires3',msg.to.id,'5')
 	end
 	if tonumber(timetoexpire) == 4 then
 		if redis:hget('expires4',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '4 days remaining until the end of the expiry date of the group. Refer to recharge the @SweetherBotAdmin. ')
+		send_large_msg(get_receiver(msg), '4 days remaining until the end of the expiry date of the group. Refer to recharge the @MrDear. ')
 		redis:hset('expires4',msg.to.id,'5')
 	end
 	if tonumber(timetoexpire) == 5 then
 		if redis:hget('expires5',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '5 days remaining until the end of the expiry date of the group. Refer to recharge the @SweetherBotAdmin. ')
+		send_large_msg(get_receiver(msg), '5 days remaining until the end of the expiry date of the group. Refer to recharge the @MrDear. ')
 		redis:hset('expires5',msg.to.id,'5')
 	end
 end
 return msg
 end
 function run(msg, matches)
-	if matches[1]:lower() == 'charge' then
+	if matches[1]:lower() == 'expire' then
 		if not is_sudo(msg) then return end
 		local time = os.time()
 		local buytime = tonumber(os.time())
@@ -64,8 +64,8 @@ function run(msg, matches)
 end
 return {
   patterns = {
-    "^[!/#]([Cc])arge (.*)$",
-	"^[!/#]([Cc]arge)$",
+    "^[!/#]([Ee]xpire) (.*)$",
+	"^[!/#]([Cc]harge)$",
   },
   run = run,
   pre_process = pre_process
