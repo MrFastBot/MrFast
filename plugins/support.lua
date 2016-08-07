953bf3cc79d2d2d0a713296dde774abc0f3ee092
UltronTG <UltronTG@gmail.com>
2016-08-07 13:01:08 +0430

    Create support.lua

---
+++ plugins/support.lua
@@ -0,0 +1,20 @@
+do
+    local function run(msg, matches)
+    local support = '1031459611'
+    local data = load_data(_config.moderation.data)
+    local name_log = user_print_name(msg.from)
+        if matches[1] == 'support' or 'tosupport' then
+        local group_link = data[tostring(support)]['settings']['set_link']
+    return "➖➖➖➖➖➖➖➖➖➖\n💢UltronTG💢 Support Group Link⤵\n➖➖➖➖➖➖➖➖➖➖\n️"..group_link.."➖➖➖➖➖➖➖➖➖➖\nClicl Here For Join To Support Group\n➖➖➖➖➖➖➖➖➖➖"
+    end
+end
+return {
+    patterns = {
+    "^[#!/]([Ss]upport)$",
+    "^[#!/]([Tt]osupport)$",
+    "^([Ss]upport)$",
+    "^([Tt]osupport)$",
+     },
+    run = run
+}
+end
