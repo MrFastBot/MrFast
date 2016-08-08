52d7ae60cabe035f35c65d0f0a7d5cb2a76a2366
UltronTG <UltronTG@gmail.com>
2016-08-08 23:07:30 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'sweether' then -- Put everything you like :)
+  if matches[1]:lower() == 'ultron' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /shield ")
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "sweether" then -- Put everything you like :)
+    if matches[2] == "ultron" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (sweether)",
-	"^[#!/]([Ss]weether)"
+    "^[#!/]([Ss]tats) ([Uu]ltron)",
+	"^[#!/]([Uu]ltron)"
     }, 
   run = run
 }
