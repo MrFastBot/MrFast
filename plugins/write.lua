fdcccaddde95d293751b1a3324cf1b82abe0bee3
UltronTG <UltronTG@gmail.com>
2016-08-09 10:50:37 +0430

    Update write.lua

--- plugins/write.lua
+++ plugins/write.lua
@@ -186,7 +186,7 @@ local function run(mag, matches)
 		a=a+1
 		result_text = result_text..a.."- "..result[a].."\n\n"
 	end
-	return result_text.."➖➖➖➖➖➖➖➖➖➖"
+	return result_text.."➖➖➖➖➖➖➖➖➖➖\nυℓтяση\n➖➖➖➖➖➖➖➖➖➖"
 end
 
 return {
