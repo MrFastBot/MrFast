b450c5cfbf634e40045392bf83f3145e73ee4861
UltronTG <UltronTG@gmail.com>
2016-08-09 13:07:49 +0430

    Update ping.lua

--- plugins/ping.lua
+++ plugins/ping.lua
@@ -1,10 +1,11 @@
 local function run(msg, matches)
-local text =  "Pong!"
+local text =  "Pong!" or "Fuck off I'm onlline" or "I'm onlline ͡° ͜ʖ ͡°" or "Pingo kir khar onlline hastam" or "Khafe onlline hastam"
 return text
 end
 return {
   patterns = {
-    '^([Pp]ing)$'
+    '^([Pp]ing)$',
+    '^[/#!]([Pp]ing)$'
 },
 run = run
 }
