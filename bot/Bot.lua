f9ecf4c8239c858b7121d1cf62b4e33ec87a25fa
AssassinTG <AssassinTGBot@gmail.com>
2016-08-19 17:23:02 +0430

    Update Bot.lua

--- bot/Bot.lua
+++ bot/Bot.lua
@@ -281,18 +281,11 @@ function create_config( )
     },
     sudo_users = {239607076,214344324},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[UltronTG v 1.7
-    
-My Team:
-@i_liked [Developer]
-@MrDear [Developer]
-
-Our channels
-Telegram.me/UltronTM [persian]
+    about_text = [[
 ]],
-    help_text_realm = [[Hmmmmm!
+    help_text_realm = [[
 ]],
-    help_text = [[Hmmmmmm!
+    help_text = [[
 ]],
 	help_text_super =[[
 ‼SuperGroup Commands⤵️
