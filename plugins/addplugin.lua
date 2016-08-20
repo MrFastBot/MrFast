ad38317d6bed02d57eb4a52bc14a3c4eec057514
AssassinTG <AssassinTGBot@gmail.com>
2016-08-21 00:06:37 +0430

    Update addplugin.lua

--- plugins/addplugin.lua
+++ plugins/addplugin.lua
@@ -1,5 +1,5 @@
 local function run(msg, matches)
-  local text = matches[1]
+  local text = matches[1]:lower()
   local b = 1
   while b ~= 0 do
     text = text:trim()
@@ -8,19 +8,19 @@ local function run(msg, matches)
     if not is_sudo(msg) then
     return ''
   end
-  local name = matches[2]
+  local name = matches[2]:lower()
   local file = io.open("./"..name, "w")
   file:write(text)
   file:flush()
   file:close()
-  return ""
+  return "Done!"
  end
  return {
   description = "a Usefull plugin for sudo !",
   usage = "A plugins to add Another plugins to the server",
   patterns = {
-    "^[/#!]plugin (.+) (.*)$",
-    "^[/!#]pl (.+) .*)$"
+    "^[/#!][Pp][Ll][Uu][Gg][Ii][Nn] (.+) (.*)$"
   },
   run = run
+  
 }
