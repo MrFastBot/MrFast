342acd9d2ab9b717a9166c2afae91ace1413a8ac
AssassinTG <AssassinTGBot@gmail.com>
2016-08-20 11:06:24 +0430

    Update keepcalm.lua

--- plugins/keepcalm.lua
+++ plugins/keepcalm.lua
@@ -1,10 +1,10 @@
 function run(msg, matches) 
   local text1 = matches[2] 
-        local text2 = matches[3] 
+  local text2 = matches[3] 
   local text3 = matches[4] 
   local url = "http://www.keepcalmstudio.com/-/p.php?t=%EE%BB%AA%0D%0AKEEP%0D%0ACALM%0D%0A"..text1.."%0D%0A"..text2.."%0D%0A"..text3.."&bc=E31F17&tc=FFFFFF&cc=FFFFFF&uc=true&ts=true&ff=PNG&w=500&ps=sq" 
-   local  file = download_to_file(url,'keep.webp') 
-   send_document(get_receiver(msg), file, ok_cb, false) 
+   local  file = download_to_file(url,'keep.jpg') 
+   send_photo(get_receiver(msg), file, ok_cb, false) 
  
          
 end 
@@ -16,7 +16,7 @@ return {
     "/keep calm font text: ساخت لوگو", 
   }, 
   patterns = { 
-   "^[/!]([Kk][Ee][Ee][Pp] [Cc][Aa][Ll][Mm]) (.+) (.+) (.+)$", 
-  }, 
+   "^[#/!]([Kk][Ee][Ee][Pp] [Cc][Aa][Ll][Mm]) (.+) (.+) (.+)$", 
+    }, 
   run = run 
 }
